41f371d4fdf7192d4d86af19d9fddf58d80984e0
Vie0n <113540258+Vie0n@users.noreply.github.com>
2022-12-16 18:17:51 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h1>Text</h1>
+  </React.StrictMode>,
+)
