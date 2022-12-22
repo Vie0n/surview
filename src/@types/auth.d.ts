caefd9a9dbe269173c5a5d53b14b2f06a0194cd1
Vie0n <113540258+Vie0n@users.noreply.github.com>
2022-12-22 15:24:16 +0100

    Add firebase email/pass auth

--- src/@types/auth.d.ts
+++ src/@types/auth.d.ts
@@ -1,11 +1,13 @@
-import type { User } from 'firebase/auth'
+import type { User, UserCredential } from 'firebase/auth'
 
 export interface IAuthContextProps {
   children: React.ReactNode
 }
 
 export type AuthContextType = {
+  createUser: (email: string, password: string) => Promise<UserCredential>,
   googleSignIn: () => void,
+  emailSignIn: (email: string, password: string) => Promise<UserCredential>,
   logOut: () => void,
   user: User | null
 }
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,11 +6,13 @@ import type {
   IAuthContextProps 
 } from '../@types/auth'
 
-import { 
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, 
   GoogleAuthProvider, 
   signInWithRedirect,
-  signOut,
   onAuthStateChanged,
+  signOut,
   User
 } from 'firebase/auth'
 
@@ -21,27 +23,31 @@ const AuthContext = createContext<AuthContextType | null>(null)
 export const AuthContextProvider: React.FC<IAuthContextProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null)
 
+  // Register with email and password
+  const createUser = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
   // Login with google
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider()
     signInWithRedirect(auth, provider)
   }
-  
-  const logOut = () => {
-    signOut(auth)
+
+  // Login with email and password
+  const emailSignIn = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password)
   }
+  
+  const logOut = () => signOut(auth)
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (currUser) => {
-      setUser(currUser)
-    })
-    return () => {
-      unsub()
-    }
+    const unsub = onAuthStateChanged(auth, (currUser) => setUser(currUser))
+    return () => unsub()
   }, [])
 
   return (
-    <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+    <AuthContext.Provider value={{ createUser, googleSignIn, emailSignIn, logOut, user }}>
       { children }
     </AuthContext.Provider>
   )
